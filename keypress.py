ee2ae4c13890704cfd2353f6f604c7cd9e4e48eb
Peyman Majidi <peyman.majidi@gmail.com>
2019-07-29 10:32:37 +0430

    update readme

--- keypress.py
+++
@@ -1,7 +0,0 @@
-import keyboard #Using module keyboard
-while True:  #making a loop
-    if keyboard.is_pressed('up'): #if key 'up' is pressed.You can use right,left,up,down and others
-        print('PRESSED ',end='')
-    else:
-        pass
-
